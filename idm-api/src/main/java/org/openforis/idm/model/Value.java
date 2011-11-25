1d85b395365678ca90cd5fd9fb4bf3bb57f2b42a
Gino Miceli <gino.miceli@fao.org>
2011-11-25 12:22:56 +0100

    Removed umplementation-specific remarks and symbol from Value interface.

--- idm-api/src/main/java/org/openforis/idm/model/Value.java
+++ idm-api/src/main/java/org/openforis/idm/model/Value.java
@@ -5,21 +5,5 @@ package org.openforis.idm.model;
  * @author M. Togna
  */
 public interface Value {
-	enum Symbol {
-		BLANK, NOT_APPLICABLE, ILLEGIBLE
-	}
-
-	/**
-	 * @return Returns the symbol.
-	 * @uml.property name="symbol" readOnly="true"
-	 */
-	Symbol getSymbol();
-
-	/**
-	 * @return Returns the remarks.
-	 * @uml.property name="remarks" readOnly="true"
-	 */
-	String getRemarks();
-
 	abstract boolean isBlank();
 }

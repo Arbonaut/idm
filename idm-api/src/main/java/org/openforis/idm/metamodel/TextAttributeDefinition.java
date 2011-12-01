7756741115f52e6e1625c721b36080d5f58a1dd4
Gino Miceli <gino.miceli@fao.org>
2011-12-01 13:59:58 +0100

    Merged memo and string into text type

--- idm-api/src/main/java/org/openforis/idm/metamodel/TextAttributeDefinition.java
+++ idm-api/src/main/java/org/openforis/idm/metamodel/TextAttributeDefinition.java
@@ -4,6 +4,10 @@ package org.openforis.idm.metamodel;
  * @author G. Miceli
  * @author M. Togna
  */
-public interface TextAttributeDefinition extends StringAttributeDefinition {
-
+public interface TextAttributeDefinition extends AttributeDefinition {
+	enum Type {
+		SHORT, MEMO 
+	}
+	
+	Type getType();
 }

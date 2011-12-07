c7cea86d8a121de57e24d67785aa74a394c25e46
Gino Miceli <gino.miceli@fao.org>
2011-12-07 16:17:39 +0100

    Removed validation and relevance state from API model objects

--- idm-api/src/main/java/org/openforis/idm/model/ModelObject.java
+++ idm-api/src/main/java/org/openforis/idm/model/ModelObject.java
@@ -1,8 +1,5 @@
 package org.openforis.idm.model;
 
-import java.util.List;
-
-import org.openforis.idm.metamodel.Check;
 import org.openforis.idm.metamodel.ModelObjectDefinition;
 
 /**
@@ -13,22 +10,4 @@ public interface ModelObject<D extends ModelObjectDefinition> {
 
 	D getDefinition();
 
-	/**
-	 * @return Returns the relevant.
-	 * @uml.property name="relevant" readOnly="true"
-	 */
-	Boolean isRelevant();
-
-	/**
-	 * @return Returns an immutable list of failed checks.
-	 */
-	List<Check> getFailedChecks();
-
-	List<Check> getErrors();
-
-	List<Check> getWarnings();
-
-	boolean hasErrors();
-
-	boolean hasWarnings();
 }

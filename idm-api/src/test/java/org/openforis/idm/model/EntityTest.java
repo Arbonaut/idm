74803f6f04593df8f0036f07aebbd4dc040978e1
Gino Miceli <gino.miceli@fao.org>
2012-01-12 11:16:19 +0100

    Fixed broken enum type adapters, unmarshall error event handling

--- idm-api/src/test/java/org/openforis/idm/model/EntityTest.java
+++ idm-api/src/test/java/org/openforis/idm/model/EntityTest.java
@@ -7,6 +7,7 @@ import java.net.URL;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openforis.idm.metamodel.Survey;
+import org.openforis.idm.metamodel.xml.InvalidIdmlException;
 import org.openforis.idm.metamodel.xml.SurveyUnmarshaller;
 
 /**
@@ -17,7 +18,7 @@ public class EntityTest {
 	private static Survey survey;
 
 	@BeforeClass
-	public static void setUp() throws IOException {
+	public static void setUp() throws IOException, InvalidIdmlException {
 		URL idm = ClassLoader.getSystemResource("test.idm.xml");
 		InputStream is = idm.openStream();
 		SurveyUnmarshaller su = new SurveyUnmarshaller();

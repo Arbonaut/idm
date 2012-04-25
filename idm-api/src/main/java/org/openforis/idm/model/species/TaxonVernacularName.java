d9d7d7b7e665ada3aa7a1dd61d375b3d08feb919
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2012-04-25 16:59:53 +0700

    we had qualifiers as List<String> which will contain qualifier1..3
    Signed-off-by: Eko Suprapto Wibowo <swdev.bali@gmail.com>

--- idm-api/src/main/java/org/openforis/idm/model/species/TaxonVernacularName.java
+++ idm-api/src/main/java/org/openforis/idm/model/species/TaxonVernacularName.java
@@ -4,21 +4,19 @@ import java.util.Collections;
 import java.util.List;
 
 /**
-*	@author G. Miceli
-*	@author M. Togna
-*   @author E. Wibowo
-*/
+ * @author G. Miceli
+ * @author M. Togna
+ * @author E. Wibowo
+ */
 public class TaxonVernacularName {
-	
+
 	private Integer id;
 	private String vernacularName;
 	private String languageCode;
 	private String languageVariety;
 	private Integer taxonSystemId;
 	private int step;
-	private String qualifier1;
-	private String qualifier2;
-	private String qualifier3;
+	private List<String> qualifiers;
 
 	public Integer getId() {
 		return id;
@@ -68,30 +66,11 @@ public class TaxonVernacularName {
 		this.step = step;
 	}
 
-	public String getQualifier1() {
-		return qualifier1;
+	public List<String> getQualifiers() {
+		return Collections.unmodifiableList(qualifiers);
 	}
 
-	public void setQualifier1(String qualifier1) {
-		this.qualifier1 = qualifier1;
+	public void setQualifiers(List<String> qualifier) {
+		this.qualifiers = qualifier;
 	}
-
-	public String getQualifier2() {
-		return qualifier2;
-	}
-
-	public void setQualifier2(String qualifier2) {
-		this.qualifier2 = qualifier2;
-	}
-
-	public String getQualifier3() {
-		return qualifier3;
-	}
-
-	public void setQualifier3(String qualifier3) {
-		this.qualifier3 = qualifier3;
-	}
-
-
-
 }

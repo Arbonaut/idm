e9485eaf9438068aded65034ac31dc779eb696e4
Stefano Ricci <stefano.ricci@fao.org>
2013-04-15 13:56:31 +0200

    Moved version number to 3.0-CR1-SNAPSHOT

--- idm-api/src/main/java/org/openforis/idm/metamodel/validation/package-info.java
+++
@@ -1,6 +0,0 @@
-@XmlSchema(namespace = "http://www.openforis.org/idml/3.0", elementFormDefault = XmlNsForm.QUALIFIED)
-package org.openforis.idm.metamodel.validation;
-
-import javax.xml.bind.annotation.XmlNsForm;
-import javax.xml.bind.annotation.XmlSchema;
-
